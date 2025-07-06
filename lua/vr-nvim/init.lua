3b15d4c4949c902971ab22da28556ca79ef4ec0d
segodimo <segodimo@gmail.com>
2025-07-06 19:14:18 -0300

    tst07

--- lua/vr-nvim/init.lua
+++ lua/vr-nvim/init.lua
@@ -1,14 +1,16 @@
 local M = {}
 
 M.run = function()
-  print("VR run")
   vim.opt.relativenumber = false
   vim.opt.number = false
   vim.cmd('set numberwidth=20')
   -- vim.cmd("term") -- abra terminal
   vim.cmd("vsplit") -- split da tela
+  vim.cmd("wincmd w") -- foco para a primeira janela
+  vim.cmd("windo diffthis") -- compare os lados
+  vim.cmd("startinsert") -- modo insert
+  print("VR is On!")
 end
-
 
 M.stop = function()
   print("VR stop")
@@ -16,7 +18,10 @@ M.stop = function()
   vim.opt.relativenumber = true
   vim.cmd('set numberwidth=4')
   -- vim.cmd("term") -- abra terminal
+  vim.cmd("windo diffoff")
   vim.cmd("q")  -- ou vim.cmd("quit")
+  vim.cmd("stopinsert")
+  print("VR is Off!")
 end
 
 M.setup = function()
