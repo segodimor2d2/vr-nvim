8b72a0face6a278e8e18446a227db7711e11f293
Your Name <segodimo@gmail.com>
2025-07-06 12:34:50 -0300

    tst02

--- lua/vr-nvim/init.lua
+++ lua/vr-nvim/init.lua
@@ -1,13 +1,24 @@
 local M = {}
 
 M.run = function()
-  print("oioio")
+  print("VR run")
+end
+
+
+M.stop = function()
+  print("VR stop")
 end
 
 M.setup = function()
-	vim.keymap.set("n", "<leader>fvr", function()
+
+	vim.keymap.set("n", "<leader>fvv", function()
 		M.run()
 	end, { desc = "on VR" })
+
+	vim.keymap.set("n", "<leader>fvs", function()
+		M.run()
+	end, { desc = "off VR" })
+
 end
 
 return M
